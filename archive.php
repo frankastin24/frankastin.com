1bc18ce8cd5e8d86f162eb34a2d335d8edfd1d6f
Frank Astin <frankastin@mine.local>
2025-02-12 09:16:15 +0000

    Added slider to portfolio

--- archive.php
+++ archive.php
@@ -2,16 +2,36 @@
 
 get_header('portfolio');
 
-
-
 ?>
+
 <div id="portfolio-hero">
+    
+    
+    <div class="slides">
+
+    <?php
+     $args = array(
+        'post_type' => 'portfolio',
+        
+    );
+    $query = new WP_Query($args);
+
+    foreach($query->posts as $portfolio) {
+        ?>
+        <div style="background-image:url(<?= get_the_post_thumbnail_url($portfolio->ID);?>)" class="slide">
+
+        </div>
+        <?php
+    }
+    ?>
+
+    </div>
+
     <div class="overlay">
         <div class="container realtive">
     <h3>PORTFOLIO</h3>
         </div>
     </div>
-
 </div>
 
 
@@ -21,8 +41,10 @@ get_header('portfolio');
 
 
 <?php 
-
+global $project_images;
+$project_images = [];
 function render_entries($year) {
+    global $project_images;
     $args = array(
         'post_type' => 'portfolio',
         'tax_query' => array(
@@ -42,6 +64,7 @@ function render_entries($year) {
         
         $query = new WP_Query($args);
         foreach ($query->posts as $portfolio) {
+            $project_images[] = get_the_post_thumbnail_url($portfolio->ID);
         ?>
             <a class="portfolio-entry  <?= strtolower(str_replace(' ', '-' ,get_the_title($portfolio->ID)));?>" href="<?= get_the_permalink($portfolio->ID); ?>" style="background-image:url(<?= get_the_post_thumbnail_url($portfolio->ID) ?>)">
             <div class="overlay">   
@@ -84,7 +107,6 @@ render_entries('2023');
 
 
 </div>
-
 <?php 
 get_footer();
 ?>
--- js/skills.js
+++ js/skills.js
@@ -1,20 +1,25 @@
 
+jQuery(($) => {
+   
+    if($('.slides').length > 0) {
+        const numSlides = $('.slide').length - 1;
+        
+        let currentSlide = 0;
 
-const skillsCanvas = document.getElementById('skills-canvas');
+        $('.slide').eq(currentSlide).addClass('active');
 
-skillsCanvas.width = window.innerWidth / 3;
-skillsCanvas.height = window.innerHeight;
-const sctx = skillsCanvas.getContext('2d');
+        setInterval(() => {
+            currentSlide ++;
+            
+            if(currentSlide == numSlides) {
+                currentSlide = 0;
+            }
 
-sctx.font = '30px sans-serif';
-sctx.fillStyle = 'white';
-sctx.fillText('SKILLS', skillsCanvas.width / 2, skillsCanvas.height / 2)
-
-sctx.beginPath();
-sctx.arc(skillsCanvas.width / 2, skillsCanvas.height / 2, 100, 0, 2 * Math.PI);
-sctx.stroke();
-
-
+            $('.active').removeClass('active');
+            $('.slide').eq(currentSlide).addClass('active');
+        },10000)
+    }
+} )
 
 
 
